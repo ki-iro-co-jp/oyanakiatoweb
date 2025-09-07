ba19a2a53c391f90cfd07fa1509a1f842fb0dc71
zb185423 <zb185423@yahoo.co.jp>
2025-09-07 11:51:06 +0900

    impl

---
+++ src/data/menus.js
@@ -0,0 +1,55 @@
+export const menuMain = [
+    // {
+    //     name: "Home",
+    //     url: "/",
+    //     highlighted: false,
+    // },
+    // {
+    //     name: "Menu",
+    //     url: "/menu",
+    //     highlighted: false,
+    // },
+    // {
+    //     name: "About Us",
+    //     url: "/about-us",
+    //     highlighted: false,
+    // },
+    {
+        name: "スポンサーの皆様",
+        url: "/gallery",
+        highlighted: false,
+    },
+    {
+        name: "アプリを使ってみる",
+        url: "https://oyanakiato.vercel.app/",
+        highlighted: true,
+    },
+];
+
+export const footerPrimary = [
+    {
+        title: "Navigatie",
+        items: [
+            { name: "home", url: "/" },
+        ],
+    },
+];
+
+export const footerSecondary = [
+    {
+        title: "support",
+        items: [
+            { name: "pricing", url: "/pricing" },
+        ],
+    },
+];
+
+export const footerTertiary = [
+    {
+        title: "Links",
+        items: [
+            { name: "privacybeleid", url: '/privacy-policy' },
+            { name: "api status", url: "/api-status" },
+        ],
+    },
+];
